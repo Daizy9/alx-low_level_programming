6eac87d7d8c1ef03a89896d1095a672922e078d0
Daizy9 <itohowo203@gmail.com>
2022-09-09 14:34:10 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * * main - main is the entry point to the program
+ * * function has no parameters
+ * * Return: 0 Always
+ **/
+int main(void)
+{
+	char letter = 'a';
+
+	for (; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+		return (0);
+}
