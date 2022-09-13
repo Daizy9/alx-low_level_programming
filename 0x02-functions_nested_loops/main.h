f6db5e6ca53b7ea92d90fd1ba1303087d8fd2131
Daizy9 <itohowo203@gmail.com>
2022-09-13 10:04:48 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef MAIN_N
+#ifindf MAIN_N
 #define MAIN_H
 int _putchar(char C);
 void print_alphabet(void);
@@ -13,6 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_tables(int n);
-#endif
 
                                   
