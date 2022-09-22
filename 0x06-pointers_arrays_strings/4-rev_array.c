a47a3b63eac2d8df0c851e736e09c74f34fa51e7
Daizy9 <itohowo203@gmail.com>
2022-09-22 05:45:58 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,13 @@
  **/
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < size/2; i++)
+	int i;
+	int t;
+
+	for (i = 0; i < n / 2; i++)
 	{
-		int temp = arr[i];
-		arr[i] = arr[size - 1 - i];
-		arr[size - 1 - i] = temp;
+		t = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = t;
 	}
 }
