c1f088943b3cf5309ede9e612e0cc111e150ff22
Daizy9 <itohowo203@gmail.com>
2022-09-22 08:49:51 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * * print_number - prints an integer
+ * * @n:.input integer parameter
+ **/
+void print_number(int n)
+{
+	int a;
+
+	_putchar("Enter an integer\n");
+	scanf("%d", &a);
+	_putchar("The integer is: %d\n", a);
+	return (0);
+}
