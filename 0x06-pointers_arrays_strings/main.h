faec02566305da705dcce89516626741476879b5
Daizy9 <itohowo203@gmail.com>
2022-09-23 01:50:17 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_M
 #define MAIN_M
 
+#include <stdio.h>
+#include <string.h>
+
+
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
