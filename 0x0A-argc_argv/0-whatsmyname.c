c1ccc346a7cd8c24072c4f30e5887aea9b10c858
Daizy9 <itohowo203@gmail.com>
2022-09-30 11:50:56 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * * @argv: An array of pointers to the arguments.
  * * Return: Always o.
  **/
-int main(int argc, char ** argv)
+int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
-	return 0;
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
 }
