8ba23fb7186bba915205ab662b49fdd141ac5681
Daizy9 <itohowo203@gmail.com>
2022-10-25 09:20:15 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,27 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 /**
- * * add_nodeint_end - adds a node at the end of a linked list
- * * @head: pointer to the first node in the list
- * * @n: data to insert in the new node created
- * * Return: pointer to the new node, or NULL if it fails
- */
+ * * add_nodeint_end - adds a new node at the end of list
+ * * @head: a pointer to a pointer to a struct
+ * * @n: holds the data for the node
+ * * Return: the address of the new element, or NULL if it failed
+ **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *next_ptr;
+	listint_t *new_node;
 
-new = malloc(sizeof(listint_t));
-if (!new)
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
 	return (NULL);
-new->n = n;
-new->next = NULL;
-if (*head == NULL)
-{
-	*head = new;
-	return (*head);
-}
-while (temp->next)
-	temp = temp->next;
-	temp->next = new;
-	return (*head);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
 	}
+	next_ptr = *head;
+	while (next_ptr->next != NULL)
+		next_ptr = next_ptr->next;
+	next_ptr->next = new_node;
+	return (new_node);
+}
